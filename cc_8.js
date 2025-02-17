65b69dd306cb39afc1fb036ec96c4250de492204
Easton <eastonmullins@usf.edu>
2025-02-16 20:14:35 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -2,7 +2,16 @@
 
 function calculateSalary(baseSalary, bonus, taxRate){
     let NetSalary = (baseSalary + bonus) - (baseSalary * taxRate);
-    console.log(`Net Salary $${NetSalary}`);
+    console.log(`Net Salary: $${NetSalary}`);
 }
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+//Task 2 - Product Price After Discount
+
+function calculateDiscount(price, discountRate){
+    let finalPrice = price - (price * discountRate);
+    console.log(`Final Price: $${finalPrice}`);
+}
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
